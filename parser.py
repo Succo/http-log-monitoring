5c3f506afeaa2e703cfab445390ecd37cef5c20a
Fabrice <fabrice.vaillant@student.ecp.fr>
2015-11-22 11:33:26 +0100

    Made the date a ptyhon time object
    This should simplufy future manipulation

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 def parse(files):
     """This create a list with the log entry
@@ -29,5 +30,8 @@ def parse(files):
             # here we match the patten against the log line
             # and them use groupdict to creat a dict object
             entry = pattern.match(line).groupdict()
+            date = entry["time"]
+            # we take the date a python date object for easier manipulation
+            entry["time"] = time.strptime(date, "%d/%b/%Y:%H:%M:%S %z")
             result.append(entry)
     return result
