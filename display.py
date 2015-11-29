e191ebc81bb1422e0a0fae221f01c7b7adfd0489
Fabrice <fabrice.vaillant@student.ecp.fr>
2015-11-29 12:43:33 +0100

    Cleaned a bit the display

--- display.py
+++ display.py
@@ -13,9 +13,16 @@ class DisplayManager():
         self.stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
+        curses.curs_set(0)
         self.stdscr.keypad(True)
+        maxY, maxX = self.stdscr.getmaxyx()
+        self.stdscr.addstr(maxY-1,0,"Http log parsing program - type anykey to exit")
 
-        # We keep the lock to avoid updating the data are being read
+        # We create a window where all the stats will be printed
+        # this way we can redraw only part of the screen every 10s
+        self.statWindow = self.stdscr.subwin(maxY-2, maxX, 0, 0)
+
+        # We keep a reference to the lock
         self.updatingDataLock = updatingDataLock
 
         # We keep a reference to the result generated (and updated) by the parser
@@ -48,7 +55,7 @@ class DisplayManager():
         # We try to acquire the lock to make the data are not being updated
         self.updatingDataLock.acquire()
 
-        self.stdscr.clear()
+        self.statWindow.clear()
         # This functions serve to remove one loop of data processing
         # sorted second argument must be a function returning a comparison key
         # so we need this function to do so
@@ -59,9 +66,9 @@ class DisplayManager():
         y = 0
         for section in sorted(self.sectionResult, key= lengthFromKey, reverse=True):
             # print the section name followed by the number of view
-            self.stdscr.addstr(y,0,"The section " + section + " has " + str(len(self.sectionResult.get(section))) + " view")
+            self.statWindow.addstr(y,0,"The section " + section + " has " + str(len(self.sectionResult.get(section))) + " view")
             # update line count
             y += 1
-        self.stdscr.refresh()
+        self.statWindow.refresh()
         self.updatingDataLock.release()
         return
